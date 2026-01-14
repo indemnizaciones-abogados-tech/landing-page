c8894537bb8b6d6edbe24e6601fc6fcee20bf04f
LuisaFernandaVivas <20979874+LuisaFernandaVivas@users.noreply.github.com>
2026-01-13 19:35:32 -0500

    Mejora palabras clave SEO -hotfix

--- next.config.mjs
+++ next.config.mjs
@@ -3,15 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   trailingSlash: false,
   async redirects() {
-    return [
-      // Redirect www to non-www
-      {
-        source: '/:path*',
-        has: [{ type: 'host', value: 'www.indemnizacionesabogados.com' }],
-        destination: 'https://indemnizacionesabogados.com/:path*',
-        permanent: true,
-      },
-    ];
+    return [];
   },
 };
 
